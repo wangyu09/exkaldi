9258902ca557c8467721c5cf86db817f57e4ec5e
wangyu <sanjay_wang@outlook.com>
2020-06-02 23:06:53 +0900

    update

--- quick_install.sh
+++ quick_install.sh
@@ -1,4 +1,6 @@
-pip uninstall exkaldi
+#!/bin/bash
+
+echo y | pip uninstall exkaldi
 
 python setup.py sdist bdist_wheel
 
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup,find_packages
 import glob
+import os
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -14,13 +15,13 @@ setup(
     author_email="wangyu@alps-lab.org",
     description="ExKaldi Automatic Speech Recognition Toolkit",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type=os.path.join("text","markdown"),
     url="https://github.com/wangyu09/exkaldi",
     packages=find_packages(),
     data_files = [
-            ("exkaldisrc/tools", glob.glob("tools/*"))
+            (os.path.join("exkaldisrc","tools"), glob.glob( os.path.join("tools","*")))
         ],
-    install_requires=["numpy>=1.16","PyAudio>=0.2", "kenlm>=0.0"],
+    install_requires=["numpy>=1.16", "PyAudio>=0.2", "kenlm>=0.0"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
