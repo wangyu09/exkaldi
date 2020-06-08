652b6b88618f9e2a1aca9631542521e17ba0a423
wangyu <sanjay_wang@outlook.com>
2020-06-08 18:13:30 +0900

    install kenlm from local resource

--- quick_install.sh
+++ quick_install.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-echo y | pip uninstall exkaldi
+echo y | pip uninstall exkaldi;
 
-for dn in "build" "dist" "exkaldi.edd-info";do
+cd src && cd kenlm || exit 1;
+python setup.py sdist bdist_wheel && cd dist && pip install * || exit 1;
+cd ../../..
+
+for dn in "build" "dist" "exkaldi.egg-info";do
     if [ -d $dn ];then
         rm -r $dn
     fi
-done && python setup.py sdist bdist_wheel && cd dist && pip install *
+done || exit 1;
+
+python setup.py sdist bdist_wheel || exit 1;
+cd dist && pip install *
+
+cd ..
+rm -r build dist exkaldi.egg-info
