19ce75af6c5fcad68fe109e9dbd7a2a9adb7da8f
wangyu <sanjay_wang@outlook.com>
2020-06-21 15:24:26 +0900

    update funtions configure

--- exkaldi/config/compute_spectrogram.py
+++ exkaldi/config/compute_spectrogram.py
@@ -1,12 +1,8 @@
 compute_spectrogram =  {
-                "--allow-downsample":["false",str],
-				"--allow-upsample":["false",str],
 				"--blackman-coeff":[0.42,float],
 				"--channel":[-1,int],
-				"--dither":[1,int],
-				"--energy-floor":[0,int],
-				"--frame-length":[25,int],
-				"--frame-shift":[10,int],
+				"--dither":[1.0,float],
+				"--energy-floor":[0,float],
 				"--max-feature-vectors":[-1,int],
 				"--min-duration":[0,int],
 				"--output-format":["kaldi",str],
@@ -14,9 +10,7 @@ compute_spectrogram =  {
 				"--raw-energy":["true",str],
 				"--remove-dc-offset":["true",str],
 				"--round-to-power-of-two":["true",str],
-				"--sample-frequency":[16000,int],
-				"--snip-edges":["false",str],
+				"--snip-edges":["true",str],
 				"--subtract-mean":["false",str],
-				"--window-type":["povey",str],
 				"--write-utt2dur":["",str]
 			} 
---
+++ exkaldi/config/gmm_decode.py
@@ -0,0 +1,11 @@
+config = {
+            "--beam-delta":[0.5,float],
+            "--delta":[0.000976562,float],
+            "--determinize-lattice":['true',str],
+            "--hash-ratio":[2,float],
+            "--minimize":['false',str],
+            "--phone-determinize":['true',str],
+            "--prune-interval":[25,int],
+            "--word-determinize":['true',str],
+            "--minimize":['false',str],
+}
