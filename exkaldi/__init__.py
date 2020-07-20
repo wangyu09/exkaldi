25201b43f73cad81fcd402a896c21d929df4b218
wangyu <sanjay_wang@outlook.com>
2020-07-21 01:09:15 +0900

    Update __init__.py
    1, remove audio module to another package.

--- exkaldi/__init__.py
+++ exkaldi/__init__.py
@@ -1,10 +1,11 @@
 from __future__ import absolute_import
 
-from exkaldi.version import version
+from exkaldi.version import info
+from exkaldi.version import set_timeout
+
 from exkaldi import utils
 from exkaldi.core import *
 from exkaldi import decode
 from exkaldi import lm
 from exkaldi import hmm
-
-#from exkaldi import audio
+from exkaldi import nn
