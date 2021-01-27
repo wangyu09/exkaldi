d0bf0c86744a8e2453615de8791dba4895afb069
Leow Chee Siang <cheesiangleow5284@gmail.com>
2021-01-27 10:33:20 +0900

    added pypi deploy to ubuntu_build_test_deploy

--- quick_install.sh
+++ quick_install.sh
@@ -12,9 +12,9 @@ function install_package(){
     pip3 install wheel setuptools
     pip3 install numpy==1.16
     python3 setup.py sdist bdist_wheel && cd dist && pip3 install *.whl || exit 1;
-    cd ..
-    
-    rm -r build dist *.egg-info
+
+    cd ..    
+    # rm -r build dist *.egg-info
 }
 
 echo y | pip3 uninstall exkaldi;
